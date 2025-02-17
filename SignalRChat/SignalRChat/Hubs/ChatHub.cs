6c045851a036e3cc23a861da78321ff0d07df1d6
Yan Macedo Leite <yan.leite@imetame.com.br>
2025-02-17 17:02:03 -0300

    subindo o projeto

---
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", "System", $"{Context.ConnectionId} joined {groupName}.");
+
+        }
+
+        public async Task SendMessageToGroup(string groupName, string user, string message)
+        {
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task LeaveGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Groups(groupName).SendAsync("ReceiveMessage", "System", $"{Context.ConnectionId} left {groupName}.");
+        }
+    }
+}
---
+++ SignalRChat/SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,76 @@
+﻿"use strict";
+
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+var group = '';
+var chooseHtmlGroup = document.getElementById('textGroup');
+var buttonA = document.getElementById('joinGroupAButton');
+var buttonB = document.getElementById('joinGroupBButton');
+
+var user = document.getElementById('userText');
+var userText = document.getElementById('userInput');
+var message = document.getElementById('messageText');
+var messageText = document.getElementById('messageInput');
+var enterButton = document.getElementById('sendButton');
+
+
+//user.style.visibility = 'hidden';
+//userText.style.visibility = 'hidden';
+//message.style.visibility = 'hidden';
+//messageText.style.visibility = 'hidden';
+//enterButton.style.visibility = 'hidden';
+
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on('ReceiveMessage', function (user, message) {
+    var li = document.createElement('li');
+    document.getElementById('messagesList').appendChild(li);
+
+    li.textContent = `${user}: ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById('sendButton').disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById('sendButton').addEventListener('click', function (event) {
+    var user = document.getElementById('userInput').value;
+    var message = document.getElementById('messageInput').value;
+    connection.invoke('SendMessage', user, message).catch(function (err) {
+        return console.log(err.toString());
+    });
+    event.preventDefault();
+});
+document.getElementById('sendGroupButton').addEventListener('click', function (event) {
+    var user = document.getElementById('userInput').value;
+    var message = document.getElementById('messageInput').value;
+    connection.invoke('SendMessageToGroup', group, user, message).catch(function (err) {
+        return console.log(err.toString());
+    });
+    event.preventDefault();
+});
+
+
+buttonA.addEventListener('click', function (event) {
+
+    connection.invoke('JoinGroup', 'Group A').catch(function (err) {
+        return console.log(err.toString());
+    });
+    event.preventDefault;
+    group = 'Group A';
+    alert('You Join to Group A');
+});
+
+buttonB.addEventListener('click', function (event) {
+    connection.invoke('JoinGroup', 'Group B').catch(function (err) {
+        return console.log(err.toString());
+    });
+    event.preventDefault;
+    group = 'Group B';
+    alert('You Join to Group B');
+})
+
